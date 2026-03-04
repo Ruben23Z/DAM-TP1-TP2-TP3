190c9ba43e4681e36a4fb05427d815a2d43e5582
Ruben23Z <rubenzhang200523@gmail.com>
2026-03-04 10:52:36 +0000

    EX3

--- src/damm/exer_3/exer_3.kt
+++ src/damm/exer_3/exer_3.kt
@@ -1,4 +1,10 @@
 package damm.exer_3
-fun main(){
 
+fun main() {
+    val height = 1000.0
+    val seq = generateSequence(height) {
+        if (it*0.6  >= 1.0) it * 0.6
+        else null
+    }
+    println("alturas da bola ${seq.take(15).joinToString("| ") { "%.2f".format(it) }}")
 }
