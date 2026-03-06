d98f7865c7fe42a1159931a7419206511fd7e8a8
Ruben23Z <rubenzhang200523@gmail.com>
2026-03-06 17:01:44 +0000

    Ex3 corrigido

--- src/dam/exer_3/exer_3.kt
+++ src/dam/exer_3/exer_3.kt
@@ -1,10 +1,13 @@
 package dam.exer_3
 
 fun main() {
-    val height = 1000.0
-    val seq = generateSequence(height) {
-        if (it*0.6  >= 1.0) it * 0.6
-        else null
-    }
-    println("alturas da bola ${seq.take(15).joinToString("| ") { "%.2f".format(it) }}")
+
+    val bounceSequence = generateSequence(100.0) { it * 0.6 }
+        .filter { it >= 1.0 }
+        .take(15)
+        .map { "%.2f".format(it) } // Format to 2 decimal places
+        .toList()
+
+    println("Sequencia de altura de Bounce:")
+    println(bounceSequence)
 }
